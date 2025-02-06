9496593675b8f756c630ac18d2e4b727f3234c4a
Isaac_Santos <isaacdsantos@hotmail.com>
2025-02-06 15:03:44 -0300

    corrigido erro do rselect adicionando lista particionada

--- rselecao.py
+++ rselecao.py
@@ -1,11 +1,19 @@
 
 def RSelect(input_list,n):
+    
+    n = len(input_list)
 
 	if n == 1:
-		return input_list[1]
+		return input_list[0]
 	
-	j = choice_pivot()
+	pivot_index = random.randint(0,n-1)
+    partitioned_list, j = partition(input_list, pivot_index)  # Particiona
 	
-	
+	if  j + 1 = i :
+        return partitioned_list[j]
+    elif j > i :
+        return RSelect(partitioned_list[:j], i)
+    else:
+        return RSelect(partitioned_list[j+1:], i - (j + 1))
 
 
