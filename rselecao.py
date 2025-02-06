94333a67aaf1c37a047ed768bbd9f1420cf46e25
Isaac_Santos <isaacdsantos@hotmail.com>
2025-02-06 16:21:59 -0300

    corrigido logica para retorno de elemento

--- rselecao.py
+++ rselecao.py
@@ -1,4 +1,4 @@
-
+import random
 
 def partition(input_list, pivot_index):
     
@@ -23,18 +23,35 @@ def partition(input_list, pivot_index):
 def RSelect(input_list,i):
     
     n = len(input_list)
-
-	if n == 1:
-		return input_list[0]
-	
-	pivot_index = random.randint(0,n-1)
+    if n == 0 :
+        return None
+    
+    if n == 1:
+        return input_list[0]
+    
+    pivot_index = random.randint(0,n-1)
     partitioned_list, j = partition(input_list, pivot_index)  # Particiona
-	
-	if  j + 1 = i :
-        return partitioned_list[j]
-    elif j > i :
+	# Caso o elemento procurado esta no lado dos menores
+    if i <= j:
         return RSelect(partitioned_list[:j], i)
+    # Caso  o elemento procurado é dado pelo proprio pivo
+    elif i == j + 1:
+        return partitioned_list[j]
+    # Caso o elemento procurado está entre os maiores
     else:
         return RSelect(partitioned_list[j+1:], i - (j + 1))
 
 
+
+lista = [3, 8, 2, 5, 1, 4, 7, 6]
+i = 3
+resultado = RSelect(lista, i)
+print(f"O {i}-ésimo menor elemento é: {resultado}")
+
+i = 1
+resultado = RSelect(lista, i)
+print(f"O {i}-ésimo menor elemento é: {resultado}")
+
+i = 8
+resultado = RSelect(lista, i)
+print(f"O {i}-ésimo menor elemento é: {resultado}")
